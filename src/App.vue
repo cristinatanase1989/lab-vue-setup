f56b6ac111029b8464e92eb41170223540f9183a
cristinatanase1989 <110465755+cristinatanase1989@users.noreply.github.com>
2022-10-11 15:35:02 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hello word</h1>
+ 
+  <HelloWorld/>
+  <ToDoItem/>
+
+</template>
+
+<script setup>
+
+import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from './components/ToDoItem.vue';
+
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+
+<div>
+
+  <button @click="nombreDeLaFuncion()">Lo que viene escrito dentro del button</button>
+</div>
+
+
+
+</template>
+
+
+<script setup>
+
+ const nombreDeLaFuncion = () =>{
+  console.log("Hello Word")
+ }
+
+
+</script>
+
+
+<style>
+
+
+</style>
